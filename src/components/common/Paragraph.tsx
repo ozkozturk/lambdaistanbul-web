aa50835ce3b54cd39442079944e02e64bec2374f
Sertaç Onay <sertac@onay.me>
2020-08-27 22:02:04 +0300

    another way of writing component

--- src/components/common/Paragraph.tsx
+++ src/components/common/Paragraph.tsx
@@ -1,16 +1,8 @@
-import React, { FC } from 'react';
 import { Typography } from 'antd';
-import { ParagraphProps } from 'antd/lib/typography/Paragraph.d';
 import styled from '@emotion/styled';
 
 const { Paragraph } = Typography;
 
-const AppParagraph: FC<ParagraphProps> = ({ children, ...rest }) => {
-  return (
-    <Paragraph {...rest}>
-      {children}
-    </Paragraph>
-  );
-};
+const AppParagraph = styled(Paragraph)``;
 
-export default styled(AppParagraph)``;
+export default AppParagraph;
